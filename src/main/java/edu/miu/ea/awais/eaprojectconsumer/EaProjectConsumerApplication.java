4dedd0dd71f8615945843797b122594474df67c4
Awais Waheed <awaiswaheed321@gmail.com>
2024-12-20 20:34:24 -0600

    Added swagger

--- src/main/java/edu/miu/ea/awais/eaprojectconsumer/EaProjectConsumerApplication.java
+++ src/main/java/edu/miu/ea/awais/eaprojectconsumer/EaProjectConsumerApplication.java
@@ -1,11 +1,20 @@
 package edu.miu.ea.awais.eaprojectconsumer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
+@OpenAPIDefinition(info = @Info(
+        title = "${spring.application.name}",
+        description = "${app.description}",
+        version = "${app.version}",
+        contact = @Contact(name = "Awais Waheed", email = "awaheed@miu.edu")
+))
 public class EaProjectConsumerApplication {
 
     public static void main(String[] args) {
